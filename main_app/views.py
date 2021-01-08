ef5e0daf49fa9ede54932578d7770d99f75c523a
DaCoPro <comerbdavid@gmail.com>
2021-01-08 12:43:58 -0800

    Complete step 3.3.

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Stuff
+from .forms import ItemForm
 
 # Create your views here.
 def index(request):
     items = Stuff.objects.all()
-    return render(request, 'index.html', {'items': items})
+    item_form = ItemForm()
+    return render(request, 'index.html', {'items': items, 'item_form': item_form})
 
+def add_item(request):
+  print('working')
+  form = ItemForm(request.POST)
+  if form.is_valid():
+    new_item = form.save(commit=False)
+    new_item.save()
+  return redirect('index')

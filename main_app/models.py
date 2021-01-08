e495a79564110315c1e1953c6ad1c72873ab64ea
DaCoPro <comerbdavid@gmail.com>
2021-01-08 11:46:03 -0800

    ABuild model, url, and view fx for index.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Stuff(models.Model):
+    name = models.CharField(max_length=50)
+    quantity = models.IntegerField(default=0)
+
+    def __str__ (self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('index')
